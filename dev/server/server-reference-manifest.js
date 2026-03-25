3e689d3cf555f962a9905cbf94347d8e3a160ff7
iamseopyeongwon <pw.seo@ebridge.co.kr>
2026-03-25 19:21:23 +0900

    Updates

--- dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"V6hL8YFpWNqJUPdskTLLOYXFj4DzH/T2STPcnxSUWmM=\"\n}"
--- dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__06.-pfn._.css","static/chunks/node_modules_137i.60._.js","static/chunks/node_modules_next_dist_0tt2wve._.js","static/chunks/components_dev_AxeCoreRunner_tsx_0~1-k14._.js"],
-    source: "dynamic"
-});
